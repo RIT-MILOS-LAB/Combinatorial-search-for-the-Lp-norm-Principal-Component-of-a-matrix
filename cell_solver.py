062dadbff92733c1acfe735978cdd1db4e857543
dgchachlakis <dimitris@mail.rit.edu>
2020-12-15 22:53:47 -0500

    The exact solution to Lp-quasinorm (p<=1) Principal-Component Analysis via exhaustive search.
    cell_solver.py solves the problem over a cell defined by a vector binary b.
    exhaustive_search_solver.py solves the problem by an exhaustive search among all possible binary vectors b.
    
    test.py illustrates the use of the exhaustive search solver.

---
+++ cell_solver.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cvxpy as cp
+
+def cell_solver(X,b,p):
+    B=np.diag(b)
+    Y=X.dot(B)
+    D=X.shape[0]
+    
+    # CVX solver starts here
+    qcp=cp.Variable(D) # Create a vector variable.
+    cost=cp.sum(cp.power(Y.T @ qcp,p))
+    constraints = [cp.norm(qcp) <= 1]
+    prob = cp.Problem(cp.Maximize(cost), constraints)
+    prob.solve()
+    q=qcp.value
+    metric=prob.value
+    
+    return q, metric 
---
+++ test.py
@@ -0,0 +1,12 @@
+import numpy as np
+from exhaustive_search_solver import *
+# D: number of rows (features)
+# N: nimber of columns (data measuremnts)
+D,N=3,10
+
+p=1/2
+X=np.random.randn(D,N)
+q, metric = exhaustive_search_solver(X , p)
+
+print(q)
+print(metric)
