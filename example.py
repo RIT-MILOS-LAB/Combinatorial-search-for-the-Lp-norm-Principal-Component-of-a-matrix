996ddc608183a86b03bddc85f161d32728ebb0d2
dgchachlakis <dimitris@mail.rit.edu>
2021-01-10 22:30:32 -0500

    python coding conventions

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import numpy as np
 import algorithm as lppc
-D=3
-N=5
-p=1/2
-X=np.random.randn(D,N)
-qopt,bopt,metopt=lppc.exact_exhaustive(X,p)
+D = 3
+N = 5
+p = 1/2
+X = np.random.randn(D, N)
+qopt, bopt, metopt = lppc.exact_exhaustive(X, p)
--- utils/solver_fixedb.py
+++ utils/solver_fixedb.py
@@ -1,13 +1,13 @@
 import numpy as np
 import cvxpy as cp
 import cvxopt 
-def solver_fixedb(X,b,p):
-    B=np.diag(b)
-    Y=X@B
-    D=X.shape[0]
-    q=cp.Variable(D) 
-    cost=cp.sum(cp.power(Y.T @ q,p))
-    constraints = [cp.norm(q) <= 1, Y.T @ q >=0 ]
+def solver_fixedb(X, b, p):
+    B = np.diag(b)
+    Y = X @ B
+    D = X.shape[0]
+    q = cp.Variable(D) 
+    cost = cp.sum(cp.power(Y.T @ q, p))
+    constraints = [cp.norm(q) <= 1, Y.T @ q >= 0 ]
     prob = cp.Problem(cp.Maximize(cost), constraints)
     try:
         prob.solve(solver = 'CVXOPT' , verbose = False)
@@ -16,4 +16,4 @@ def solver_fixedb(X,b,p):
     except:
         q = np.zeros(D)
         metric = 0
-    return q,metric 
+    return q, metric 
